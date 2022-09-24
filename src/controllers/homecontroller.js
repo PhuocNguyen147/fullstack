aa999febfac1727f063af0823da0af9d954daf1e
PhuocNguyen147 <nguyen.huuphuoc147@gmail.com>
2022-09-24 16:05:26 +0700

    ket noi database voi sequelize

--- src/controllers/homecontroller.js
+++ src/controllers/homecontroller.js
@@ -1,6 +1,20 @@
-let getHomePage = (req, res) => { // đây là tạo fuction tham số là req và res
+import db from '../models/index';
+
+let getHomePage = async (req, res) => { // đây là tạo fuction tham số là req và res
+
     // return res.send("Hello controller");
-    return res.render('homepages.ejs');
+    try {
+        let data = await db.User.findAll();
+        console.log("---------------------");
+        console.log(data);
+        console.log("---------------------");
+        return res.render('homepages.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 
 let getme = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import bodyParser from "body-parser"; // lấy được tham sô mà phía client gửi cho chúng ta
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require('dotenv').config(); // gọi đến hàm config của dotenv để chạy lệnh process.env 
 // let dotenv
 let app = express();
@@ -13,6 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
+
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
     console.log("Backend Nodejs DONE!!!" + port)
