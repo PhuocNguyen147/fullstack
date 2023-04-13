b4d9c1f9873514aec900db89bd06d7581fe3f5fb
PhuocNguyen147 <nguyen.huuphuoc147@gmail.com>
2023-04-13 15:46:48 +0700

    uploadimage

--- src/server.js
+++ src/server.js
@@ -10,8 +10,11 @@ require('dotenv').config(); // gọi đến hàm config của dotenv để chạ
 let app = express();
 //config app
 app.use(cors({ origin: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
--- src/services/userService.js
+++ src/services/userService.js
@@ -117,7 +117,8 @@ let createNewUser = (data) => {
                     phonenumber: data.phonenumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
                 resolve({
                     errCode: 0,
@@ -178,7 +179,10 @@ let updateUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phonenumber = data.phonenumber
-
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
+                // truyền ảnh 
                 await user.save();
                 resolve({
                     errCode: 0,
